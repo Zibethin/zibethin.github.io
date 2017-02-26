c58ac8f549ce0fd2028b98cf63c59f314d4ff8ea
Zibethin <nguyenththuong@gmail.com>
2017-02-26 20:52:12 +0000

    Re-designed website completely from scratch
    New design

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,13 @@
-console.log('This would be the main JS file.');
+var contact = document.getElementById("google");
+var contactDetail = document.getElementById("contact-detail");
+
+contact.onclick = function () {
+    if (contactDetail.style.visibility == "hidden") {
+        contactDetail.style.visibility = "visible";
+        contactDetail.style.opacity = "100";
+    }
+    else {
+        contactDetail.style.visibility = "hidden";
+        contactDetail.style.opacity = "0";
+    }
+}
