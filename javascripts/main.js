a6f21c915b06cd7f97c64e6021d567932b7f9b53
Zibethin <nguyenththuong@gmail.com>
2020-04-21 23:54:09 +0100

    change top level navigation and logo
    - now features hamburger menu for <640px
    - logo is now part of top level navigation
    - navigation menu items now live in flex boxes
    - fixed some alignment and font issues

--- javascripts/main.js
+++ javascripts/main.js
@@ -11,3 +11,14 @@ contact.onclick = function () {
         contactDetail.style.opacity = "0";
     }
 }
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function showMenu() {
+  var x = document.getElementById("menu-right");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
